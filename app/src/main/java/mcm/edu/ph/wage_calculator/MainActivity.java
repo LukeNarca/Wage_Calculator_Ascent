310fb2ce36e1c2797b840253f2840fae7037a3d3
LukeNarca <75514862+LukeNarca@users.noreply.github.com>
2021-01-28 16:20:54 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/wage_calculator/MainActivity.java
@@ -0,0 +1,53 @@
+package mcm.edu.ph.wage_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnCompute;
+        btnCompute = findViewById(R.id.btnCompute);
+        btnCompute.setOnClickListener((View.OnClickListener) this);
+
+    }
+
+    public void onClick(View v) {
+        EditText inputvalue;
+        TextView txtanswer;
+
+        inputvalue = findViewById(R.id.inputvalue);
+        txtanswer = findViewById(R.id.txtanswer);
+
+        int workHours = 8;
+        int hourlyWage = 100;
+        int overTimeWage = 130;
+
+        Double value = 0.0;
+        Double answer = 0.0;
+        if(value> workHours){
+           answer= (hourlyWage*workHours) + ((value-8)*(hourlyWage*overTimeWage));
+        }
+        else {
+            answer = (value * hourlyWage);
+        }
+
+        value = Double.parseDouble(inputvalue.getText().toString());
+
+        switch (v.getId()) {
+            case R.id.btnCompute:
+                txtanswer.setText(Double.toString(answer));
+
+        }
+    }
+
+}
